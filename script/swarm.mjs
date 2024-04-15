ab83561b3c61da823f14202a8c2dfea6079c96db
Paul Potsides <paul@koble.ai>
2024-04-15 16:45:50 +0200

    Override token alpha with migration for tokens with alpha = 0. Use tint and alpha from token.

--- script/swarm.mjs
+++ script/swarm.mjs
@@ -31,6 +31,7 @@ const SETTING_HP_REDUCE_ATTRIBUTE_VALUE = "attributeHpValue";
 const SETTING_HP_REDUCE_ATTRIBUTE_MAX = "attributeHpMax";
 const SETTING_FADE_TIME = "fadeTime";
 const SETTING_STOP_TIME = "stopTime";
+const SETTING_MIGRATED_TO = "migratedTo";
 const theta = 0.01;
 const SIGMA = 5;
 const GAMMA = 1000;
@@ -80,14 +81,16 @@ function getHealthEstimate(token){
 }
 
  
-let SWARMS = {};
+const SWARMS = {};
 // TODO: Remove debug accessor
 window.SWARMS = SWARMS;
 
 export default class Swarm {
-    constructor( token, number ){
+    constructor( token, document = token.document ){
+        const number = document.getFlag(MOD_NAME, SWARM_SIZE_FLAG)
         this.t = 0;
-        this.token  = token;
+        this.token = token;
+        this.document = document
         this.currentHPPercent = this.calculateHPPercent();  // Calculate current HP percent
         this.number = this.determineVisibleSprites(this.currentHPPercent, number);  // Determine initial number of visible sprites
         this.maxSprites = number;  // Store the maximum number of sprites
@@ -99,20 +102,35 @@ export default class Swarm {
         this.layer = new PIXI.Container();
 
         // this.randomRotation = true;        
-        this.faded  = token.document.hidden;
-        this.visible= (this.faded)?0:this.number;
+        this.faded  = document.hidden;
+        this.visible= (this.faded) ? 0 : this.number;
+        this.spriteAlpha = document.alpha;
 
-        
-        this.layer.elevation = (token.document.getFlag(MOD_NAME, OVER_FLAG)?10000:0);
+        const swarm = this;
+
+        Object.defineProperty(document, "alpha", {
+          get() {
+            return 0;
+          },
+          set(v) {
+            if(!document.hidden) {
+              swarm.spriteAlpha = v
+            }
+          },
+          configurable: true,
+          enumerable: true,
+        });
+        token.alpha = 0;
+
+        this.layer.elevation = (document.getFlag(MOD_NAME, OVER_FLAG)?10000:0);
         this.layer.sort = 120; // Above tiles at 100
-        this.layer.alpha = token.isVisible ? 1 : 0;
+        this.layer.alpha = token.isVisible ? this.spriteAlpha : 0;
         canvas.primary.addChild(this.layer);
                 
         this.created = false;
-        //this.createSprites(number, token, this.layer);
         
         this.tick = new PIXI.Ticker();
-        let anim = token.document.getFlag(MOD_NAME, ANIM_TYPE_FLAG);
+        const anim = document.getFlag(MOD_NAME, ANIM_TYPE_FLAG);
         this.set_destinations = this.circular;
         switch(anim){
           case ANIM_TYPE_CIRCULAR:
@@ -137,17 +155,18 @@ export default class Swarm {
         }
         this.tick.add( this.anim.bind(this) );
         this.tick.start();
+        Hooks.call('createSwarm', this)
     }
     
-    async createSprites( number, token, layer ){
-        let use_random_image    = token.actor.prototypeToken.randomImg;
-        let hidden = token.document.hidden;
+    async createSprites( number ){
+        const use_random_image = this.token.actor.prototypeToken.randomImg;
+        const hidden = this.document.hidden;
 
-        let images = [];
+        const images = [];
         if (use_random_image){            
             images = await swarm_socket.executeAsGM("wildcards",token.id);                
         }else{
-          images.push(token.document.texture.src);
+          images.push(this.document.texture.src);
         }
 
         for(let i=0;i<number;++i){
@@ -161,38 +180,33 @@ export default class Swarm {
             s.anchor.set(.5);
             
             // Sprites initial position, a random position within this tokens area
-            s.x = token.x + Math.random()*token.w;
-            s.y = token.y + Math.random()*token.h;
+            s.x = this.token.x + Math.random() * this.token.w;
+            s.y = this.token.y + Math.random() * this.token.h;
             // Hidden initially?
             s.alpha = (hidden)?0:1;
 
-            // A callback to get correct aspect ratio, and to start the video
-            let scale = ()=>{              
-                // Get the largest dimention, and scale around that
-                let smax = Math.max(s.texture.width, s.texture.height);
-                s.scale.x = token.document.texture.scaleX * canvas.grid.size / smax;
-                s.scale.y = token.document.texture.scaleY * canvas.grid.size / smax;
-                
+            // A callback to start the video
+            const start = () => {       
                 // Check if the texture selected is a video, and potentially start it
-                let src = s.texture.baseTexture.resource.source;
+                const src = s.texture.baseTexture.resource.source;
                 src.loop = true;
                 src.muted = true; // Autostarting videos must explicitly be muted (chrome restriction)
                 if (src.play) src.play();
             };
             if (s.texture.baseTexture.valid){
-              scale();
-            }else{
-              s.texture.baseTexture.on('loaded', scale);
+              start();
+            } else {
+              s.texture.baseTexture.on('loaded', start);
             }            
             // Set the initial destination to its initial position
             this.dest.push({x:s.x, y:s.y});
             this.sprites.push(s);
-            let sf = token.document.getFlag(MOD_NAME, SWARM_SPEED_FLAG);
+            let sf = this.document.getFlag(MOD_NAME, SWARM_SPEED_FLAG);
             if (sf===undefined) sf = 1;
             // Add 50% of the speed as variability on each sprites speed
             this.speeds.push( sf*.5 + sf * Math.random()*0.5 )
             // Add this sprite to the correct layer
-            layer.addChild(s);
+            this.layer.addChild(s);
         }
     }
 
@@ -207,42 +221,85 @@ export default class Swarm {
         return Math.max(minSprites, Math.round(hpPercent * maxNumber));
     }
 
+    determineStep(ms) {
+      const fd = game.settings.get(MOD_NAME, SETTING_FADE_TIME);
+      const count = Math.abs(this.visible - this.number);
+      // step, corresponding to the module setting "fade time", also, prevent division by zero
+      return (fd==0)?(count):(ms*count)/(fd*1000);
+    }
+
     /**
      * The main animation callback for this swarm
      * @param {Number} t Time fraction of the current fps
      */
     anim(t){
         if (!this.created){
-          this.createSprites(this.maxSprites, this.token, this.layer);  // Use maxSprites instead of number
+          this.createSprites(this.maxSprites);  // Use maxSprites instead of number
         }
 
         t = Math.min(t,2.0);// Cap frame skip to two frames
         // Milliseconds elapsed, as calculated using the "time" fraction and current fps
-        let ms = t*1000*(1.0/this.tick.FPS);
-        let fd = game.settings.get(MOD_NAME, SETTING_FADE_TIME);
-        // step, corresponding to the module setting "fade time", also, prevent division by zero
-        let step = (fd==0)?(this.number):(ms*this.number)/(fd*1000);
+        const ms = t*1000*(1.0/this.tick.FPS);
+       
 
-        if (this.faded && (this.visible>0)){ 
-            // We should be faded/hidden, and we still have critters visible
-            this.visible -= step;
-            this.sprites.forEach((s,i)=>{s.alpha = (i>=this.visible)?0:1});
+        const getScale = s => {
+          // Get the largest dimension, and scale around that
+          const smax = Math.max(s.texture.width, s.texture.height);
+          const x = this.document.texture.scaleX * canvas.grid.size / smax;
+          const y = this.document.texture.scaleY * canvas.grid.size / smax;
+          return {x, y}
         }
-        if (!this.faded && (this.visible<this.number)){ 
-            // We should be visible, and we still have critters hidden
-            this.visible += step;
-            this.sprites.forEach((s,i)=>{s.alpha = (i>this.visible)?0:1});
-        }
+
+        let updateSprites =
+          this.tint != this.document.texture.tint;
 
         const currentHPPercent = this.calculateHPPercent();
         if (currentHPPercent !== this.currentHPPercent || !this.created) {
-            this.currentHPPercent = currentHPPercent;
-            this.number = this.determineVisibleSprites(currentHPPercent, this.maxSprites);
-            // Adjust visibility based on the number of "surviving" sprites
-            // Start from last sprite as formations would move to the front.
-            this.sprites.toReversed().forEach((s, i) => {
-                s.alpha = (i < this.number) ? 1 : 0;
-            });
+          this.currentHPPercent = currentHPPercent;
+          this.number = this.determineVisibleSprites(currentHPPercent, this.maxSprites);
+          this.step = this.determineStep(ms)
+          updateSprites = true;
+        }
+
+        if(this.step === null) {
+          this.step = this.determineStep(ms)
+        }
+
+        if (Math.round(this.visible) !== this.number) {
+          updateSprites = true;
+          if (this.visible > this.number) {
+            this.visible -= this.step;
+            if (this.visible < this.number) {
+              this.visible = this.number;
+            }
+          } else {
+            this.visible += this.step;
+            if (this.visible > this.number) {
+              this.visible = this.number;
+            }
+          }
+        }
+
+        if(!updateSprites && this.sprites.length) {
+          if (typeof this.scale === 'undefined') {
+            updateSprites = true;
+          } else {
+            const scale = getScale(this.sprites[0]);
+            updateSprites = this.scale.x !== scale.x || this.scale.y !== scale.y;
+          }
+        }
+
+        if(updateSprites) {
+          const remaining = Math.round(this.maxSprites - this.visible);
+          this.sprites.forEach((s,i) => {
+            s.alpha = i >= remaining ? this.spriteAlpha : 0;
+            this.scale = getScale(s);
+            s.scale.x = this.scale.x;
+            s.scale.y = this.scale.y;
+            if(this.document.texture.tint) {
+              this.tint = s.tint = this.document.texture.tint;
+            }
+          });
         }
 
         // Calling the animation specific method, set_destination
@@ -258,6 +315,13 @@ export default class Swarm {
 
     hide(hidden){
         this.faded = hidden;
+        // Clear step to be recalcuated on next tick
+        this.step = null;
+        if(hidden) {
+          this.number = 0;
+        } else {
+          this.number = this.determineVisibleSprites(this.currentHPPercent, this.maxSprites)
+        }
     }
 
     // TODO: HP interaction    
@@ -265,11 +329,19 @@ export default class Swarm {
     }
       
     destroy(){
+        Hooks.call('preDestroySwarm', this)
         for (let s of this.sprites){
             s.destroy();
         }
         this.tick.destroy();        
         this.layer.destroy();
+        Object.defineProperty(this.document, 'alpha', {
+          value: this.spriteAlpha, 
+          configurable: true,
+          enumerable: true,
+          writable: true
+        })
+        Hooks.call('destroySwarm', this)
     }
 
     skitter(ms) {
@@ -432,52 +504,100 @@ Hooks.on('canvasTearDown', (a,b)=>{
 });
 
 function deleteSwarmOnToken(token){
-    if (token.id in SWARMS){
-        SWARMS[token.id].destroy();
+  const swarm = SWARMS[token.id]
+    if (swarm){
+        swarm.destroy();
         delete SWARMS[token.id];
     }
 }
-function createSwarmOnToken(token){
-  SWARMS[token.id] = new Swarm(token, token.document.getFlag(MOD_NAME, SWARM_SIZE_FLAG));
-  if (!game.user.isGM){
-    token.alpha = 0;
-  }
+
+function createSwarmOnToken(token, document){
+  deleteSwarmOnToken(token);
+  Hooks.call('preCreateSwarm', token, document)
+  SWARMS[token.id] = new Swarm(token, document);
 }
+
 function hideSwarmOnToken(token, hide){
     if (token.id in SWARMS){
         SWARMS[token.id].hide(hide);
     }
 }
 
+/**
+ * @param {*} changes 
+ * @returns If any swarm related flag was in this update
+ */
+const swarmNeedsRefresh = changes => {
+  if(!changes) {
+    return false;
+  }
+  if (changes.flags?.[MOD_NAME]) {
+    return true;
+  }
+  if (typeof changes.alpha === "number") {
+    return true;
+  }
+  if (changes.texture) {
+    return true;
+  }
+  return false;
+}
 
+Hooks.on('preUpdateToken', (document, changes) => {
+  if (swarmNeedsRefresh(changes)) {   
+    deleteSwarmOnToken(document);
+  }
+})
 
-Hooks.on('updateToken', (token, change, options, user_id)=>{
-    if (change?.flags?.swarm){   // If any swarm related flag was in this update
+Hooks.on('updateToken', (document, changes) => {
+    if(document.flags?.[MOD_NAME]?.[SWARM_FLAG]) {
+      // if (swarmNeedsRefresh(changes)) {
+      //     createSwarmOnToken(canvas.tokens.get(document.id));
+      // }
+      if (changes.hidden != undefined){
+          hideSwarmOnToken(document, changes.hidden);
+      }
+    }
+});
+
+Hooks.on('renderTokenConfig', (renderConfig) => {
+  const document = renderConfig.token;
+  const onDrawToken = (token) => {
+    if(token.document.id === document.id) {
+      deleteSwarmOnToken(token);
+      if(token.document.flags?.[MOD_NAME]?.[SWARM_FLAG]) {
+          createSwarmOnToken(token);
+      }
+    }
+  }
+  Hooks.on('drawToken', onDrawToken)
+  Hooks.once('closeTokenConfig', (closeConfig) => {
+    if(closeConfig.token.id === document.id) {
+      Hooks.off('drawToken', onDrawToken);
+      const {token: document} = closeConfig;
+      const token = document.object;
+      if(token) {
         deleteSwarmOnToken(token);
-        if (token.flags?.[MOD_NAME]?.[SWARM_FLAG]){
-            createSwarmOnToken(canvas.tokens.get(token.id));
+        if(document.flags?.[MOD_NAME]?.[SWARM_FLAG]) {
+            createSwarmOnToken(token, document);
         }
+      }
     }
-
-    if (change.hidden != undefined && token.flags?.[MOD_NAME]?.[SWARM_FLAG]){
-        hideSwarmOnToken(token, change.hidden);
-    }
-    
+  })
 });
 
-
-// TODO: Add HP interaction
-Hooks.on('updateActor', (actor, change, options, user_id)=>{
-    let val = change.data?.attributes?.hp?.value;
-    if (val == undefined){
-        val = change.system?.attributes?.hp?.value;
-    }
-    if (val != undefined){
-      let tk = actor.token;
-      let mx = actor.data.data.attributes.hp.max;
-      let hp = 100*val/mx;
-    }
-});
+// // TODO: Add HP interaction
+// Hooks.on('updateActor', (actor, change, options, user_id)=>{
+//     let val = change.data?.attributes?.hp?.value;
+//     if (val == undefined){
+//         val = change.system?.attributes?.hp?.value;
+//     }
+//     if (val != undefined){
+//       let tk = actor.token;
+//       let mx = actor.data.data.attributes.hp.max;
+//       let hp = 100*val/mx;
+//     }
+// });
 
 
 // Delete token
@@ -487,17 +607,35 @@ Hooks.on('deleteToken', (token, options, user_id)=>{
     delete SWARMS[token.id];
   }
 })
+
+const isSwarmingToken = (t) =>!!t.document.getFlag(MOD_NAME,SWARM_FLAG);
+const getSwarmingTokens = () => canvas.tokens.placeables.filter(isSwarmingToken);
+
 // Create token
 Hooks.on('createToken', (token, options, user_id)=>{
-  //console.warn("Create Token", token, options);
   if (token.getFlag(MOD_NAME, SWARM_FLAG)===true){
     createSwarmOnToken(token.object);
   }
 });
 
-const getSwarmingTokens = () => canvas.tokens.placeables.filter((t)=>!!t.document.getFlag(MOD_NAME,SWARM_FLAG)) 
- 
-Hooks.on("canvasReady", ()=> {
+Hooks.on("ready", async () => {
+    if(game.settings.get(MOD_NAME, SETTING_MIGRATED_TO) < 11.0) {
+     ui.notifications.notify(`Migrating to Swarms version 11.  Please don't refresh your browser.`)
+     const actors = game.actors.filter(a => a.prototypeToken.getFlag(MOD_NAME, SWARM_FLAG) && a.prototypeToken.alpha === 0);
+     if(actors.length) {
+        await Promise.all(actors.map(async actor => await actor.prototypeToken.update({alpha: 1})))
+     }
+     let tokenCount = 0;
+     await Promise.all(game.scenes.map(async scene => {
+        const tokens = scene.tokens.filter(token => token.getFlag(MOD_NAME, SWARM_FLAG) && token.alpha === 0)
+        if(tokens.length) {
+          await Promise.all(tokens.map( async token => await token.update({alpha: 1})))
+          tokenCount += tokens.length;
+        }
+     }))
+     await game.settings.set(MOD_NAME, SETTING_MIGRATED_TO, 11.0);
+     ui.notifications.notify(`Migration to Swarms version 11 complete. Updated ${actors.length} actor(s) and ${tokenCount} token(s).`);
+    }
     // Scene loaded.
     for (let s of getSwarmingTokens()){
         createSwarmOnToken(s);
@@ -509,14 +647,15 @@ Hooks.on('sightRefresh', (canvasVisibility) => {
     const swarmedTokens = getSwarmingTokens();
     for(let t of swarmedTokens) {
       const swarm = SWARMS[t.id];
-      swarm.layer.alpha = t.isVisible ? 1 : 0;
+      if(swarm) { // Swarm might not exist if just been updated
+        swarm.layer.alpha = t.isVisible ? swarm.spriteAlpha : 0;
+      }
     }
   }
 })
  
  // Settings:
  Hooks.once("init", () => {
-  
     game.settings.register(MOD_NAME, SETTING_HP_REDUCE, {
         name: "Reduce swarm with HP",
         hint: "Reduce the swarm as HP decreases, requires support for your system",
@@ -558,9 +697,15 @@ Hooks.on('sightRefresh', (canvasVisibility) => {
       default: 5.0
   });
 
+  game.settings.register(MOD_NAME, SETTING_MIGRATED_TO, {
+      name: "Migrations",
+      scope: 'world',
+      config: false,
+      type: Number,
+      default: 0
+  });
   
  });
- 
 
 
  /*
